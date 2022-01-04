b8ce86180aa9873e54771c9c759df6e62eeb4052
Cristóbal Veas <cristobal.veas@pm.me>
2022-01-04 12:16:07 -0300

    build: bump spigot-api, bungeecord-chat and item-nbt-api versions
    This allows us to use the 1.18 API, and also makes the plugin compatible with that version!

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,13 +33,13 @@ dependencies {
     implementation("javax.inject:javax.inject:1")
 
     implementation("cl.bgmp:command-framework-bukkit:1.0.3-SNAPSHOT")
-    implementation("de.tr7zw:item-nbt-api:2.8.0")
+    implementation("de.tr7zw:item-nbt-api:2.9.0")
     implementation("fr.mrmicky:fastboard:1.2.1")
     implementation("org.bstats:bstats-bukkit-lite:1.7")
     implementation("com.github.cryptomorin:XSeries:8.5.0.1")
 
-    compileOnly("org.spigotmc:spigot-api:1.17.1-R0.1-SNAPSHOT")
-    compileOnly("net.md-5:bungeecord-chat:1.17-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.18.1-R0.1-SNAPSHOT")
+    compileOnly("net.md-5:bungeecord-chat:1.18-R0.1-SNAPSHOT")
     compileOnly("com.mojang:authlib:1.5.25")
     compileOnly("me.clip:placeholderapi:2.10.10")
     compileOnly("com.arcaniax:HeadDatabase-API:1.3.1")
