f0e9435a186dd789f63689d05fc6492657acaf8f
Cristóbal Veas <cristobal.veas@pm.me>
2022-09-06 14:22:34 -0400

    chore: update copyright notice

--- src/main/java/team/devblook/akropolis/util/TextUtil.java
+++ src/main/java/team/devblook/akropolis/util/TextUtil.java
@@ -1,6 +1,8 @@
 /*
  * This file is part of Akropolis
  *
+ * Copyright (c) 2022 DevBlook Team and others
+ *
  * Akropolis free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
