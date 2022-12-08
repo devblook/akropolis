784efedd7339abc25b676f5dfa8f183db065f049
Cristóbal Veas <cristobal.veas@pm.me>
2022-12-07 23:54:03 -0300

    feat: create NametagHelper class with methods to manage teams name tags easily

---
+++ src/main/java/team/devblook/akropolis/module/modules/visual/nametag/NametagHelper.java
@@ -0,0 +1,64 @@
+/*
+ * This file is part of Akropolis
+ *
+ * Copyright (c) 2022 DevBlook Team and others
+ *
+ * Akropolis free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Akropolis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Akropolis. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package team.devblook.akropolis.module.modules.visual.nametag;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import net.megavex.scoreboardlibrary.api.team.ScoreboardTeam;
+import net.megavex.scoreboardlibrary.api.team.TeamInfo;
+import net.megavex.scoreboardlibrary.api.team.TeamManager;
+import org.bukkit.entity.Player;
+import team.devblook.akropolis.AkropolisPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NametagHelper {
+    private final TeamManager mainTeamManager;
+    private final Map<String, TeamInfo> teamInfos;
+
+    // TODO: refresh placeholders every x seconds
+    public NametagHelper() {
+        this.mainTeamManager = AkropolisPlugin.getInstance().getScoreboardManager().teamManager();
+        this.teamInfos = new HashMap<>();
+    }
+
+    public void createFormat(String formatName, Component prefix, TextColor color, Component suffix, Player player) {
+        ScoreboardTeam team = mainTeamManager.createIfAbsent(formatName);
+
+        team.teamManager().addPlayer(player);
+
+        TeamInfo teamInfo = team.globalInfo();
+
+        teamInfo.prefix(prefix);
+        teamInfo.playerColor(NamedTextColor.nearestTo(color));
+        teamInfo.suffix(suffix);
+
+        teamInfo.addEntry(player.getName());
+        teamInfos.put(formatName, teamInfo);
+    }
+
+    public void deleteFormat(String formatName, String playerName) {
+        if (!teamInfos.containsKey(formatName)) return;
+
+        teamInfos.get(formatName).removeEntry(playerName);
+    }
+}
