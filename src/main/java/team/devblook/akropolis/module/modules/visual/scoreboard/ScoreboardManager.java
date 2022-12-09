b20cc4af5645dab3d1ea3deb85692ceab19eb31c
Cristóbal Veas <cristobal.veas@pm.me>
2022-12-09 17:56:12 -0300

    docs: add some todo's for some features that don't work on reload

--- src/main/java/team/devblook/akropolis/module/modules/visual/scoreboard/ScoreboardManager.java
+++ src/main/java/team/devblook/akropolis/module/modules/visual/scoreboard/ScoreboardManager.java
@@ -43,6 +43,7 @@ public class ScoreboardManager extends Module {
     private String title;
     private List<String> lines;
 
+    // TODO: also make this work on reload :(
     public ScoreboardManager(AkropolisPlugin plugin) {
         super(plugin, ModuleType.SCOREBOARD);
     }
