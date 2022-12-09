7d2c578707ac3b412d5df259cfe7a0fe6a019c88
Cristóbal Veas <cristobal.veas@pm.me>
2022-12-09 17:43:28 -0300

    feat: use NametagUpdateTask to refresh the placeholders in nametags

--- src/main/java/team/devblook/akropolis/module/modules/visual/nametag/NametagManager.java
+++ src/main/java/team/devblook/akropolis/module/modules/visual/nametag/NametagManager.java
@@ -21,6 +21,7 @@ package team.devblook.akropolis.module.modules.visual.nametag;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
@@ -34,9 +35,11 @@ import team.devblook.akropolis.module.ModuleType;
 import team.devblook.akropolis.util.PlaceholderUtil;
 
 public class NametagManager extends Module {
-    ConfigurationSection format;
-    public NametagHelper nametagHelper;
+    private ConfigurationSection format;
+    private NametagHelper nametagHelper;
+    private int nametagTask;
 
+    // TODO: make this work on reload
     public NametagManager(AkropolisPlugin plugin) {
         super(plugin, ModuleType.NAMETAG);
     }
@@ -47,30 +50,43 @@ public class NametagManager extends Module {
 
         format = config.getConfigurationSection("nametag.format");
         nametagHelper = new NametagHelper();
+
+        if (config.getBoolean("nametag.refresh.enabled")) {
+            nametagTask = Bukkit.getScheduler().scheduleSyncRepeatingTask(getPlugin(), new NametagUpdateTask(this, nametagHelper), 0L,
+                    config.getLong("nametag.refresh.rate"));
+        }
+
+        Bukkit.getScheduler().runTaskLaterAsynchronously(getPlugin(), () ->
+                Bukkit.getOnlinePlayers().forEach(player -> {
+                    Component prefix = PlaceholderUtil.setPlaceholders(format.getString("prefix"), player);
+                    TextColor color = TextColor.fromHexString(format.getString("name_color", "#FFFFFF"));
+                    Component suffix = PlaceholderUtil.setPlaceholders(format.getString("suffix"), player);
+
+                    nametagHelper.createFormat(prefix, color, suffix, player);
+                }), 20L);
     }
 
     @Override
     public void onDisable() {
-
+        Bukkit.getScheduler().cancelTask(nametagTask);
+        Bukkit.getOnlinePlayers().forEach(nametagHelper::deleteFormat);
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
 
-        if (inDisabledWorld(player.getWorld())) return;
-
         Component prefix = PlaceholderUtil.setPlaceholders(format.getString("prefix"), player);
         TextColor color = TextColor.fromHexString(format.getString("name_color", "#FFFFFF"));
         Component suffix = PlaceholderUtil.setPlaceholders(format.getString("suffix"), player);
 
-        nametagHelper.createFormat(player.getName(), prefix, color, suffix, player);
+        nametagHelper.createFormat(prefix, color, suffix, player);
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
-        String playerName = event.getPlayer().getName();
+        Player player = event.getPlayer();
 
-        nametagHelper.deleteFormat(playerName, playerName);
+        nametagHelper.deleteFormat(player);
     }
 }
