67f371a4c51d2107a9c56ed2849e4ce7d8cef9da
Cristóbal Veas <cristobal.veas@proton.me>
2023-03-02 13:39:27 -0300

    fix: tell which is the name of the material we couldn't parse

--- src/main/java/team/devblook/akropolis/util/ItemStackBuilder.java
+++ src/main/java/team/devblook/akropolis/util/ItemStackBuilder.java
@@ -130,7 +130,8 @@ public class ItemStackBuilder {
         Optional<XMaterial> xmaterial = XMaterial.matchXMaterial(rawMaterial);
 
         if (xmaterial.isEmpty()) {
-            PLUGIN.getLogger().severe("Could not parse material for item!");
+            PLUGIN.getLogger().severe("Could not parse material '" + rawMaterial + "'");
+            PLUGIN.getLogger().severe("Please check your config.yml!");
             return MALFORMED_ITEM;
         }
 
