480e38a1671b6873d5f2ae7982696d25c8163851
Cristóbal Veas <cristobal.veas@proton.me>
2023-04-08 10:36:45 -0400

    build: bump scoreboard-library version to 2.0.0-RC7

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,9 +26,9 @@ repositories {
 dependencies {
     implementation("javax.inject:javax.inject:1")
 
-    implementation("com.github.MegavexNetwork.scoreboard-library:scoreboard-library-api:2.0.0-RC6")
-    runtimeOnly("com.github.MegavexNetwork.scoreboard-library:scoreboard-library-implementation:2.0.0-RC6")
-    runtimeOnly("com.github.MegavexNetwork.scoreboard-library:scoreboard-library-v1_19_R2:2.0.0-RC6")
+    implementation("com.github.MegavexNetwork.scoreboard-library:scoreboard-library-api:2.0.0-RC7")
+    runtimeOnly("com.github.MegavexNetwork.scoreboard-library:scoreboard-library-implementation:2.0.0-RC7")
+    runtimeOnly("com.github.MegavexNetwork.scoreboard-library:scoreboard-library-v1_19_R2:2.0.0-RC7")
 
     compileOnly("net.kyori:adventure-text-minimessage:4.13.0")
     compileOnly("net.kyori:adventure-api:4.13.0")
