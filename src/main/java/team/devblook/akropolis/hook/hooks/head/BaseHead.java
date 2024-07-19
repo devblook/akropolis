ea36aed5774a099594e99f1a33c037b846a07021
Call4han <ccasarider@gmail.com>
2024-07-18 19:23:34 -0500

    style: spaces.

--- src/main/java/team/devblook/akropolis/hook/hooks/head/BaseHead.java
+++ src/main/java/team/devblook/akropolis/hook/hooks/head/BaseHead.java
@@ -34,40 +34,40 @@ import java.util.Map;
 import java.util.UUID;
 
 public class BaseHead implements PluginHook, HeadHook {
-  private AkropolisPlugin plugin;
-  private Map<String, ItemStack> cache;
+    private AkropolisPlugin plugin;
+    private Map<String, ItemStack> cache;
 
-  @Override
-  public void onEnable(AkropolisPlugin plugin) {
-    this.plugin = plugin;
-    cache = new HashMap<>();
-  }
-
-  @Override
-  public ItemStack getHead(String data) {
-    if (cache.containsKey(data)) return cache.get(data);
-
-    ItemStack head = XMaterial.PLAYER_HEAD.parseItem();
-
-    if (head == null) {
-      plugin.getLogger().severe("Could not parse head!");
-      return XMaterial.SKELETON_SKULL.parseItem();
+    @Override
+    public void onEnable(AkropolisPlugin plugin) {
+        this.plugin = plugin;
+        cache = new HashMap<>();
     }
 
-    SkullMeta meta = (SkullMeta) head.getItemMeta();
-    if (meta == null) {
-      plugin.getLogger().severe("Could not parse head meta!");
-      return head;
+    @Override
+    public ItemStack getHead(String data) {
+        if (cache.containsKey(data)) return cache.get(data);
+
+        ItemStack head = XMaterial.PLAYER_HEAD.parseItem();
+
+        if (head == null) {
+            plugin.getLogger().severe("Could not parse head!");
+            return XMaterial.SKELETON_SKULL.parseItem();
+        }
+
+        SkullMeta meta = (SkullMeta) head.getItemMeta();
+        if (meta == null) {
+            plugin.getLogger().severe("Could not parse head meta!");
+            return head;
+        }
+        head.editMeta(SkullMeta.class, skullMeta -> {
+            PlayerProfile profile = Bukkit.createProfile(UUID.randomUUID(), null);
+            Collection<ProfileProperty> properties = profile.getProperties();
+            properties.clear();
+            properties.add(new ProfileProperty("textures", data, ""));
+            profile.setProperties(properties);
+            skullMeta.setPlayerProfile(profile);
+        });
+        cache.put(data, head);
+        return head;
     }
-    head.editMeta(SkullMeta.class, skullMeta -> {
-      PlayerProfile profile = Bukkit.createProfile(UUID.randomUUID(), null);
-      Collection<ProfileProperty> properties = profile.getProperties();
-      properties.clear();
-      properties.add(new ProfileProperty("textures", data, ""));
-      profile.setProperties(properties);
-      skullMeta.setPlayerProfile(profile);
-    });
-    cache.put(data, head);
-    return head;
-  }
 }
