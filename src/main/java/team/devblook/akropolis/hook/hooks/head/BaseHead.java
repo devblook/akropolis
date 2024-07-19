520891346ab5a938d4e56da84381ddd90991fa52
Call4han <ccasarider@gmail.com>
2024-07-18 19:15:41 -0500

    fix: problem with heads base64.

--- src/main/java/team/devblook/akropolis/hook/hooks/head/BaseHead.java
+++ src/main/java/team/devblook/akropolis/hook/hooks/head/BaseHead.java
@@ -20,60 +20,54 @@
 package team.devblook.akropolis.hook.hooks.head;
 
 import com.cryptomorin.xseries.XMaterial;
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.properties.Property;
+import com.destroystokyo.paper.profile.PlayerProfile;
+import com.destroystokyo.paper.profile.ProfileProperty;
+import org.bukkit.Bukkit;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 import team.devblook.akropolis.AkropolisPlugin;
 import team.devblook.akropolis.hook.PluginHook;
 
-import java.lang.reflect.Field;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public class BaseHead implements PluginHook, HeadHook {
-    private AkropolisPlugin plugin;
-    private Map<String, ItemStack> cache;
+  private AkropolisPlugin plugin;
+  private Map<String, ItemStack> cache;
 
-    @Override
-    public void onEnable(AkropolisPlugin plugin) {
-        this.plugin = plugin;
-        cache = new HashMap<>();
+  @Override
+  public void onEnable(AkropolisPlugin plugin) {
+    this.plugin = plugin;
+    cache = new HashMap<>();
+  }
+
+  @Override
+  public ItemStack getHead(String data) {
+    if (cache.containsKey(data)) return cache.get(data);
+
+    ItemStack head = XMaterial.PLAYER_HEAD.parseItem();
+
+    if (head == null) {
+      plugin.getLogger().severe("Could not parse head!");
+      return XMaterial.SKELETON_SKULL.parseItem();
     }
 
-    @Override
-    public ItemStack getHead(String data) {
-        if (cache.containsKey(data)) return cache.get(data);
-
-        ItemStack head = XMaterial.PLAYER_HEAD.parseItem();
-
-        if (head == null) {
-            plugin.getLogger().severe("Could not parse head!");
-            return XMaterial.SKELETON_SKULL.parseItem();
-        }
-
-        SkullMeta meta = (SkullMeta) head.getItemMeta();
-        GameProfile profile = new GameProfile(UUID.randomUUID(), "");
-
-        profile.getProperties().put("textures", new Property("textures", data));
-        Field profileField;
-
-        try {
-            if (meta == null) {
-                plugin.getLogger().severe("Could not parse head meta!");
-                return head;
-            }
-
-            profileField = meta.getClass().getDeclaredField("profile");
-            profileField.setAccessible(true);
-            profileField.set(meta, profile);
-        } catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
-            e.printStackTrace();
-        }
-
-        head.setItemMeta(meta);
-        cache.put(data, head);
-        return head;
+    SkullMeta meta = (SkullMeta) head.getItemMeta();
+    if (meta == null) {
+      plugin.getLogger().severe("Could not parse head meta!");
+      return head;
     }
+    head.editMeta(SkullMeta.class, skullMeta -> {
+      PlayerProfile profile = Bukkit.createProfile(UUID.randomUUID(), null);
+      Collection<ProfileProperty> properties = profile.getProperties();
+      properties.clear();
+      properties.add(new ProfileProperty("textures", data, ""));
+      profile.setProperties(properties);
+      skullMeta.setPlayerProfile(profile);
+    });
+    cache.put(data, head);
+    return head;
+  }
 }
