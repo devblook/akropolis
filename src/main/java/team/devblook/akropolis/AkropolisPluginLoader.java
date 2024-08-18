75a8ba9fc2a3f95d75c8ebf384a187d49cad5fa1
Cristóbal Veas <cristobal.veas@proton.me>
2024-08-17 23:47:48 -0400

    build: bump XSeries version inside of the plugin loader
    Signed-off-by: Cristóbal Veas <cristobal.veas@proton.me>

--- src/main/java/team/devblook/akropolis/AkropolisPluginLoader.java
+++ src/main/java/team/devblook/akropolis/AkropolisPluginLoader.java
@@ -32,7 +32,7 @@ public class AkropolisPluginLoader implements PluginLoader {
     @Override
     public void classloader(PluginClasspathBuilder classpathBuilder) {
         MavenLibraryResolver resolver = new MavenLibraryResolver();
-        resolver.addDependency(new Dependency(new DefaultArtifact("com.github.cryptomorin:XSeries:11.2.0"), null));
+        resolver.addDependency(new Dependency(new DefaultArtifact("com.github.cryptomorin:XSeries:11.2.1"), null));
         resolver.addRepository(new RemoteRepository.Builder("central", "default", "https://repo1.maven.org/maven2/").build());
 
         classpathBuilder.addLibrary(resolver);
