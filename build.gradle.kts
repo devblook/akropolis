6cab9dcd4d33fc3bc5507067bd1f5138993cfef6
Cristóbal Veas <cristobal.veas@proton.me>
2025-03-25 09:19:52 -0300

    build: bump paperweight-userdev to 2.0.0-beta.16 and shadow to 9.0.0-beta11

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
-    id("com.gradleup.shadow") version ("8.3.5")
-    id("io.papermc.paperweight.userdev") version ("2.0.0-beta.10")
+    id("com.gradleup.shadow") version ("9.0.0-beta11")
+    id("io.papermc.paperweight.userdev") version ("2.0.0-beta.16")
 }
 
 group = "team.devblook"
