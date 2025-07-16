9e3fcfd4d01b1b3a5a26986ee2f2895c9e0bcc2b
kamilstaszewski <25387651+kamilstaszewski@users.noreply.github.com>
2025-07-16 19:48:56 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ java {
 }
 
 repositories {
+    mavenCentral() // 🔧 Dodano do obsługi XSeries 13.3.2
     maven("https://oss.sonatype.org/content/groups/public/")
     maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
     maven("https://repo.codemc.org/repository/maven-public")
@@ -37,34 +38,3 @@ dependencies {
     compileOnly("com.mojang:authlib:1.5.25")
     compileOnly("me.clip:placeholderapi:2.11.6")
     compileOnly("com.arcaniax:HeadDatabase-API:1.3.2")
-    compileOnly("com.github.cryptomorin:XSeries:13.3.3")
-    compileOnly("io.github.miniplaceholders:miniplaceholders-api:2.3.0")
-    compileOnly("com.github.koca2000:NoteBlockAPI:1.6.2")
-}
-
-configurations.implementation {
-    exclude("org.bukkit", "bukkit")
-}
-
-tasks {
-    processResources {
-        filesMatching("paper-plugin.yml") {
-            expand("version" to project.version)
-        }
-    }
-
-    shadowJar {
-        archiveClassifier.set("")
-        archiveFileName.set("Akropolis-${project.version}.jar")
-
-        minimize {
-            exclude(dependency("net.megavex:.*:.*"))
-        }
-
-        relocate("net.megavex.scoreboardlibrary", "${libsPackage}.scoreboardlibrary")
-    }
-
-    withType<JavaCompile> {
-        options.encoding = "UTF-8"
-    }
-}
