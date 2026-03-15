9f017d7586ec7a03d0070cb79d4bf6a7d861c4e0
Fritx22 <57330546+Fritx22@users.noreply.github.com>
2026-03-15 02:12:27 -0300

    fix: use safe tag resolvers for chat messages

--- src/main/java/me/zetastormy/akropolis/util/text/TextUtil.java
+++ src/main/java/me/zetastormy/akropolis/util/text/TextUtil.java
@@ -20,6 +20,7 @@
 package me.zetastormy.akropolis.util.text;
 
 import org.bukkit.Color;
+import org.jetbrains.annotations.NotNull;
 
 import io.github.miniplaceholders.api.MiniPlaceholders;
 import net.kyori.adventure.pointer.Pointered;
@@ -27,9 +28,27 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import net.kyori.adventure.text.minimessage.tag.standard.StandardTags;
 
 public class TextUtil {
     private static final MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
+     private static final MiniMessage MINI_MESSAGE_USER_INPUT = MiniMessage.builder().tags(
+        TagResolver.builder().resolvers(
+            StandardTags.color(),
+            StandardTags.decorations(),
+            StandardTags.gradient(),
+            StandardTags.hoverEvent(),
+            StandardTags.keybind(),
+            StandardTags.newline(),
+            StandardTags.pride(),
+            StandardTags.rainbow(),
+            StandardTags.reset(),
+            StandardTags.shadowColor(),
+            StandardTags.sprite(),
+            StandardTags.transition(),
+            StandardTags.translatable(),
+            StandardTags.translatableFallback()
+        ).build()).build();
 
     private static boolean miniplaceholders = false;
 
@@ -56,8 +75,31 @@ public class TextUtil {
       return MINI_MESSAGE.deserialize(message, target, resolver);
     }
 
+    public static @NotNull Component parseUserInput(final @NotNull String message) {
+        return MINI_MESSAGE_USER_INPUT.deserialize(message);
+    }
+
+    public static @NotNull Component parseUserInput(
+        final @NotNull String message,
+        final @NotNull TagResolver resolver
+    ) {
+        return MINI_MESSAGE_USER_INPUT.deserialize(message, resolver);
+    }
+
+    public static @NotNull Component parseUserInput(
+        final @NotNull String message,
+        final @NotNull Pointered target,
+        final @NotNull TagResolver... resolver
+    ) {
+        return MINI_MESSAGE_USER_INPUT.deserialize(message, target, resolver);
+    }
+
     public static String raw(Component message) {
         return MINI_MESSAGE.serialize(message).replaceAll("\\\\<", "<");
+    }
+
+    public static String rawUserInput(final @NotNull Component message) {
+        return MINI_MESSAGE_USER_INPUT.serialize(message).replaceAll("\\\\<", "<");
     }
 
     public static Component replace(Component message, String pattern, Component replacement) {
